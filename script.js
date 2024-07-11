9b62fc69622cda84f3b7b5f9f513474ec2081ed5
Sergio Garcia Neto <88346250+sergio-gn@users.noreply.github.com>
2024-07-12 00:53:47 +1000

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,29 @@ document.addEventListener('DOMContentLoaded', () => {
         'https://cat-bounce.com/',
         'http://endless.horse/',
         'https://eelslap.com/',
-        'https://ffffidget.com/'
+        'https://ffffidget.com/',
+        'https://heeeeeeeey.com/',
+        'https://strobe.cool/',
+        'https://www.pudim.com.br/',
+        'https://hackertyper.com/',
+        'http://www.staggeringbeauty.com/',
+        'http://www.donothingfor2minutes.com/',
+        'https://llamafont.com/',
+        'https://zombo.com/',
+        'https://termo.pt/',
+        'https://quickdraw.withgoogle.com/',
+        'https://songtrivia2.io/',
+        'https://chickenonaraft.com/',
+        'https://theuselessweb.com/',
+        'https://metazooa.com/',
+        'https://www.chosic.com/random-songs-generator-with-links-to-spotify-and-youtube/',
+        'https://randommer.io/random-movies',
+        'https://www.astrology.com/compatibility/fortune-cookie.html',
+        'http://weavesilk.com/',
+        'https://haxiomic.github.io/GPU-Fluid-Experiments/html5/?q=Medium',
+        'https://www.neogeocity.com/',
+        'https://savethesounds.info/'
+        
     ];
 
     const anchor = document.getElementById('random-link');
